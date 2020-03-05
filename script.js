617f1be5c13a9de2144c228672701bb6327ac537
kabirmandal <kabir.mandal@gmail.com>
2020-03-05 10:49:29 +0600

    updated

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-$(document).ready(function(){
-    $("input").on('keyup blur',function(){
-       var deposit = $("#deposit").val(); //B1
-       var minterest = $("#minterest").val();
-       var dinterest = $("#dinterest").val();
-       var mothly_profit = deposit*minterest/100+parseFloat(deposit); $("#monthly_profit").text(mothly_profit);
-       var P1Y = deposit*(1+0.48/269)^(1*269); $("#1YP").text(P1Y);
+$(document).ready(function () {
+    $("input").on('keyup blur', function () {
+        var deposit = $("#deposit").val(); //B1
+        var minterest = $("#minterest").val();
+        var dinterest = $("#dinterest").val();
+        var mothly_profit = deposit * minterest / 100 + parseFloat(deposit);
+        $("#monthly_profit").text(mothly_profit);
+        var pw1 = 1 + 0.48 / 269;
+        var pw2 = 1 * 269;
+        var pw = Math.pow(pw1, pw2);
+        var P1Y = deposit*pw; $("#1YP").text(P1Y.toFixed(2));
+        
     });
 });
