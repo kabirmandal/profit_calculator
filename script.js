69f2db33abd533d6387b3cb4e3e72db4c9b09a45
kabirmandal <kabir.mandal@gmail.com>
2020-03-07 11:39:38 +0600

    update

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 $(document).ready(function () {
     $("input").on("keyup blur change", function () {
-        var amount = $("#amount").val(); // amount
-       // console.log(amount);
-        var mpp = $("#mpp").val(); // Monthly profit percent
-        var ytg = $("#ytg").val(); // years to grow
+        var amount = parseFloat($("#amount").val()); // amount
+        var mpp = parseFloat($("#mpp").val()); // Monthly profit percent
+        var ytg = parseFloat($("#ytg").val()); // years to grow
         var iic = $("#iic").val(); // Interest is Compounded
         function calc(x,y){
-        // =B1*(1+((0.48*B4)/269))^(B4*269)
+        
         var m = 1+x/269;
         var m = amount*m;
         var n = y*269;
         var n = Math.pow(m,n);
-        //console.log(n);
         }
         function cal(x,y){
-           var dd = amount*(1+x/269) ** (y*269);
-           //console.log(dd);
+           var dd = amount*(1+x/269) ** (y*269);           
            var dd = parseInt(dd.toFixed(2));
            return dd;
         }
-       // cal(0.48,1);
+        function ca(B1,B4){
+            var dd = B1*(1+((0.48*B4)/269)) ** (B4*269);
+            return parseInt(dd);
+         }
+        function ytgcal(B1,B4){
+            var dd = B1*(1+((0.48*B4)/269)) ** (B4*269)+B1;  console.log(dd);
+            return parseInt(dd);
+           
+        }
+       
         if (amount !== 'undefined') {
             $("#showamount").text(amount);
             $("#showytg").text(ytg);
@@ -27,16 +33,20 @@ $(document).ready(function () {
             $("#mnp").val(amount*0.04);
             $("#mgp").val(parseFloat(amount)+amount*0.04);
             
-            $("#npy1").val(cal(0.48,1));
-            $("#npy2").val(cal(0.96,2));
-            $("#npy3").val(cal(1.48,3));
+            var np1 = cal(0.48,1);
+            $("#npy1").val(np1);
+            var np2 = cal(0.96,2);
+            $("#npy2").val(np2);
+            var np3 = cal(1.48,3);
+            $("#npy3").val(np3);
 
-            $("#gpy1").val(cal(0.48,3)+0.04);
-            $("#gpy2").val(cal(0.96,3)+0.04);
-            $("#gpy3").val(cal(1.48,3)+0.04);
+            $("#gpy1").val(np1);
+            $("#gpy2").val(Number(amount)+Number(np2));
+            $("#gpy3").val(Number(amount)+Number(np3));
 
 
-        }
-        
+            $("#ytgnp").val(ca(amount,ytg));
+            $("#ytggp").val(ytgcal(amount,ytg));
+        }        
     });
 });
